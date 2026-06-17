a1ea12249cc40f742261bd5c037080bcdc0aac43
Rudroju Manideep <manideeprudroju007@gmail.com>
2026-06-17 10:40:15 +0530

    web app completed

---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+system_prompt = (
+    "You are a helpful assistant for answering questions about medical conditions. Use the following retrieved documents to answer the question. If you don't know the answer, say you don't know. Use three sentences maximum to answer the question."
+    "\n\n"
+    "{context}"
+)
